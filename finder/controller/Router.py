2d8cb25af04579da1a7d53114bfad82040b05dc8
Davasny <davasny@gmail.com>
2018-03-18 13:07:27 +0100

    Changed api view names

--- finder/controller/Router.py
+++ finder/controller/Router.py
@@ -9,9 +9,9 @@ app.add_url_rule('/index', view_func=MainHandler.IndexView.as_view('index'))
 
 
 """ API Handler """
-app.add_url_rule('/api/timetable/hours', view_func=APIHandler.TimetableHours.as_view('timetable_hours'))
-app.add_url_rule('/api/timetable/teachers', view_func=APIHandler.TimetableTeachers.as_view('timetable_teachers'))
-app.add_url_rule('/api/timetable/groups', view_func=APIHandler.TimetableGroups.as_view('timetable_groups'))
-app.add_url_rule('/api/timetable/subjects', view_func=APIHandler.TimetableSubjects.as_view('timetable_subjects'))
-app.add_url_rule('/api/timetable/classrooms', view_func=APIHandler.TimetableClassrooms.as_view('timetable_classrooms'))
+app.add_url_rule('/api/timetable/hours', view_func=APIHandler.TimetableHours.as_view('api_timetable_hours'))
+app.add_url_rule('/api/timetable/teachers', view_func=APIHandler.TimetableTeachers.as_view('api_timetable_teachers'))
+app.add_url_rule('/api/timetable/groups', view_func=APIHandler.TimetableGroups.as_view('api_timetable_groups'))
+app.add_url_rule('/api/timetable/subjects', view_func=APIHandler.TimetableSubjects.as_view('api_timetable_subjects'))
+app.add_url_rule('/api/timetable/classrooms', view_func=APIHandler.TimetableClassrooms.as_view('api_timetable_classrooms'))
 app.add_url_rule('/api/timetable/weekdays', view_func=APIHandler.TimetableWeekDays.as_view('api_timetable_weekdays'))
